d317e69a20cbfdbd0b51ea64b0a930c0b6879d2d
formotion17 <iban.upv@gmail.com>
2023-07-28 16:09:30 +0200

    Recorremos temporada y victorias/derrotas

--- src/main/java/temporadas/ClasificacionTemporada.java
+++ src/main/java/temporadas/ClasificacionTemporada.java
@@ -1,8 +1,13 @@
 package temporadas;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+import org.bson.Document;
+
 import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
@@ -13,48 +18,123 @@ import Utilidad.Util;
 
 public class ClasificacionTemporada extends BaseController{
 	
+	private static ArrayList<Equipo> listaEquipos = new ArrayList<>();
+	private static MongoClient mongo;
+	private static MongoDatabase db;
+	
 	public static void main(String[] args) throws UnknownHostException{
 		
 		System.out.println("INICAMOS LA APLICACION PARA LAS CLASIFICACIONES DE TEMPORADA");
 		
-		MongoClient mongo = Util.crearConexion();
-		
-		DBCursor cursor;
-		int i=0;
+		mongo = Util.crearConexion();
 		
 		if(mongo!=null) {
 			
-			for(Temporadas temporadas : Temporadas.values()) {
-				
-				i++;
-				String temporada =temporadas.toString();
-				System.out.println(i+"	TEMPORADA: "+temporada+"\n");
-				
-				MongoDatabase db = mongo.getDatabase("NBA");
-				
-				MongoCursor<String> c = db.getCollection(temporada).
-						distinct("equipoLocal.nombre", String.class).iterator();
-				
-				if("season20052006".equals(temporada)) {
-					System.out.println("paramos");
-				}
-				while(c.hasNext()) {
-					String nombreEquipo = c.next().replace("/", " ");
-					System.out.println(nombreEquipo);
-					if(i<=4) {
-						System.out.println(" Conferencia: "+ListaEquipos.findConferenciaAntiguaByTeam(nombreEquipo)+
-								"	Division: "+ListaEquipos.findDivisionAntiguaByTeam(nombreEquipo)+"	"+nombreEquipo);
-					}else {
-						System.out.println(" Conferencia: "+ListaEquipos.findConferenciaNuevaByTeam(nombreEquipo)+
-								"	Division: "+ListaEquipos.findDivisionNuevaByTeam(nombreEquipo)+"	"+nombreEquipo);
-					}
-				}
-				
-				System.out.println("\n\n");
-			}
+			// Recogemos los equipos de la tempoarada, pasamos el year que empieza la temporada
+			// ya que si es a partir de 2004/2005 empiezan las nuevas divisiones
+			recogerEquiposTemporada("season20002001",2000);
 			
+			MongoCursor<Document> listaPartidos=devolverPartidos("season20002001");
+
+            while (listaPartidos.hasNext()) {
+                Document document = listaPartidos.next();
+                
+                Equipo equipoLocal = devolverEquipo((String) ((Document) document.get("equipoLocal")).get("nombre"));
+                int tanteoLocal = Integer.parseInt((String) ((Document) document.get("equipoLocal")).get("tanteo"));
+                
+                Equipo equipoVisitante = devolverEquipo((String) ((Document) document.get("equipoVisitante")).get("nombre"));                
+                int tanteoVisitante = Integer.parseInt((String) ((Document) document.get("equipoVisitante")).get("tanteo"));
+                
+                if(tanteoLocal>tanteoVisitante) {
+                	
+                	equipoLocal.addVictoriaLocal();
+                	equipoLocal.addVictoria();
+                	equipoVisitante.addDerrotaVisitante();
+                	equipoVisitante.addDerrota();
+                	
+                	if(equipoLocal.getDivision().equals(equipoVisitante.getDivision())) {
+                    	equipoLocal.addVictoriaDivisionLocal();
+                    	equipoVisitante.addDerrotaDivisionVisitante();
+                	}
+                	if(equipoLocal.getConferencia().equals(equipoVisitante.getConferencia())) {
+                		equipoLocal.addVictoriaConferenciaLocal();
+                		equipoVisitante.addDerrotaConferenciaVisitante();
+                	}
+                	
+                }else{
+
+                	equipoLocal.addDerrotaLocal();
+                	equipoVisitante.addVictoriaVisitante();
+                	equipoLocal.addDerrota();
+                	equipoVisitante.addVictoria();
+                	
+                	if(equipoLocal.getDivision().equals(equipoVisitante.getDivision())) {
+                    	equipoLocal.addDerrotaDivisionLocal();
+                    	equipoVisitante.addVictoriaDivisionVisitante();
+                	}
+                	if(equipoLocal.getConferencia().equals(equipoVisitante.getConferencia())) {
+                		equipoLocal.addDerrotaConferenciaLocal();
+                		equipoVisitante.addVictoriaConferenciaVisitante();
+                	}
+                }
+            }
+		}
+		
+		System.out.println("paramos");
+		System.out.println(listaEquipos.get(0).toString());
+	}
+	
+	private static MongoCursor<Document> devolverPartidos(String temporada){
+		MongoCollection<Document> collection = db.getCollection(temporada);
+        
+        // Crear el filtro para la consulta
+        Document filter = new Document("playOff", false);
+		
+		// Realizar la consulta para recuperar solo el campo "nombre"
+        MongoCursor<Document> cursor = collection.find(filter).projection(
+        		new Document("equipoLocal.nombre", 1)
+        		.append("equipoLocal.tanteo",1)
+        		.append("equipoVisitante.nombre", 1)
+        		.append("equipoVisitante.tanteo", 1)
+        		).iterator();
+        return cursor;
+	}
+	
+	private static void recogerEquiposTemporada(String temporada,int year) {
+		System.out.println("	TEMPORADA: "+temporada+"\n");
+		
+		db = mongo.getDatabase("NBA");
+		
+		MongoCursor<String> c = db.getCollection(temporada).
+				distinct("equipoLocal.nombre", String.class).iterator();
+		
+		
+		while(c.hasNext()) {
+			String nombreEquipo = c.next().replace("/", " ");
+			if(year<2004) {
 				
+				listaEquipos.add(new Equipo(nombreEquipo,
+								ListaEquipos.findConferenciaAntiguaByTeam(nombreEquipo),
+								ListaEquipos.findDivisionAntiguaByTeam(nombreEquipo)));
+			}else {
+				listaEquipos.add(new Equipo(nombreEquipo,
+						ListaEquipos.findConferenciaNuevaByTeam(nombreEquipo),
+						ListaEquipos.findDivisionNuevaByTeam(nombreEquipo)));
+			}
 		}
 	}
 
+	private static Equipo devolverEquipo(String nombre) {
+		for(Equipo equipo:listaEquipos) {
+			if(nombre.equals(equipo.getNombre())) {
+				return equipo;
+			}
+		}
+		return null;
+	}
+	
+	
+	
+	
+	
 }
--- src/main/java/temporadas/Equipo.java
+++ src/main/java/temporadas/Equipo.java
@@ -8,4 +8,99 @@ public class Equipo {
 	private String nombre;
 	private String conferencia;
 	private String division;
+	
+	private int victorias;
+	private int derrotas;
+	private int victoriasLocal;
+	private int victoriasVisitante;
+	private int derrotasLocal;
+	private int derrotasVisitante;
+	private float porcentajeVictorias;
+	
+	private int victoriasDivisionLocal;
+	private int victoriasDivisionVisitante;
+	private int derrotasDivisionLocal;
+	private int derrotasDivisionVisitante;
+	
+	private int victoriasConferenciaLocal;
+	private int victoriasConferenciaVisitante;
+	private int derrotasConferenciaLoca;
+	private int derrotasConferenciaVisitante;
+	
+	public Equipo (String nombre, String conferencia, String division) {
+		this.nombre=nombre;
+		this.conferencia=conferencia;
+		this.division=division;
+	}
+	
+	public void addVictoriasLocal() {
+		this.victoriasLocal++;
+	}
+	
+	public void addVictoriasVisitante() {
+		this.victoriasVisitante++;
+	}
+	
+	public void addDerrotasLocal() {
+		this.derrotasLocal++;
+	}
+	
+	public void addDerrotasVisitante() {
+		this.derrotasVisitante++;
+	}
+	
+	public void addVictoriaDivisionLocal(){
+		this.victoriasDivisionLocal++;
+	}
+	public void addVictoriaDivisionVisitante(){
+		this.victoriasDivisionVisitante++;
+	}
+	
+	public void addDerrotaDivisionLocal(){
+		this.derrotasDivisionLocal++;
+	}
+	
+	public void addDerrotaDivisionVisitante() {
+		this.derrotasDivisionVisitante++;
+	}
+	
+	public void addVictoriaConferenciaLocal() {
+		this.victoriasConferenciaLocal++;
+	}
+	
+	public void addVictoriaConferenciaVisitante() {
+		this.victoriasConferenciaVisitante++;
+	}
+	
+	public void addDerrotaConferenciaLocal() {
+		this.derrotasConferenciaLoca++;
+	}
+	
+	public void addDerrotaConferenciaVisitante() {
+		this.derrotasConferenciaVisitante++;
+	}
+	
+	public void addVictoria() {
+		this.victorias++;
+	}
+	
+	public void addDerrota() {
+		this.derrotas++;
+	}
+	
+	public void addVictoriaLocal() {
+		this.victoriasLocal++;
+	}
+	
+	public void addVictoriaVisitante(){
+		this.victoriasVisitante++;
+	}
+	
+	public void addDerrotaLocal() {
+		this.derrotasLocal++;
+	}
+	
+	public void addDerrotaVisitante() {
+		this.derrotasVisitante++;
+	}
 }
